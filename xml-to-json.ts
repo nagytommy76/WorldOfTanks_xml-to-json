f04b1ec5561a428bc6dc5a3c3a4052fa616178df
nagytommy76 <nagytommy76@gmail.com>
2025-11-03 20:10:46 +0100

    Init XMLParser

---
+++ xml-to-json.ts
@@ -0,0 +1,11 @@
+import fs from 'fs'
+import path from 'path'
+import { XMLParser } from 'fast-xml-parser'
+
+const parser = new XMLParser({
+   ignoreAttributes: false,
+   allowBooleanAttributes: true,
+   attributeNamePrefix: '@_',
+   trimValues: true,
+   parseTagValue: false,
+})
