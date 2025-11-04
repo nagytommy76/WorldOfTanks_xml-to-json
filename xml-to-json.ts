989e59a7f2b7ad4ea11185226448db4f53ac35e3
nagytommy76 <nagytommy76@gmail.com>
2025-11-04 20:14:56 +0100

    Set crew roles

--- xml-to-json.ts
+++ xml-to-json.ts
@@ -1,6 +1,8 @@
 import fs from 'fs'
 import path from 'path'
 import { XMLParser } from 'fast-xml-parser'
+import { toStringArray } from './src/utils/xmlHelper'
+import { ICrew } from '@Types/Vehicle'
 
 const parser = new XMLParser({
    ignoreAttributes: false,
@@ -9,3 +11,35 @@ const parser = new XMLParser({
    trimValues: true,
    parseTagValue: false,
 })
+
+const filePath = path.join('./XML/A182_T803.xml')
+const xmlString = fs.readFileSync(filePath, 'utf-8')
+const convertedRawJSON = parser.parse(xmlString)
+
+const fileName = path.basename(filePath)
+
+// fs.writeFileSync(path.join(`vehicles.json`), JSON.stringify(convertedJSON, null, 2), 'utf8')
+
+function GetCrewData(rawJSON: any) {
+   const crewData = []
+
+   if (rawJSON?.crew) {
+      // console.log('RAW: ', rawJSON?.crew)
+      for (const [key, value] of Object.entries(rawJSON.crew)) {
+         console.log(key, value)
+         crewData.push({
+            primary: key,
+            secondary: toStringArray(value),
+         })
+      }
+   }
+
+   console.log(crewData)
+}
+
+// console.log(convertedRawJSON[fileName])
+// console.log(fileName)
+
+GetCrewData(convertedRawJSON[fileName])
+
+console.log('RAN')
