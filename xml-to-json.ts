5f8a7880ffa985e393078da1241b9bb2951d7d2b
nagytommy76 <nagytommy76@gmail.com>
2025-11-05 20:18:51 +0100

    add functions
    Add ReturnSpeedLimits, ReturnCamoValues functions

--- xml-to-json.ts
+++ xml-to-json.ts
@@ -1,8 +1,10 @@
 import fs from 'fs'
 import path from 'path'
 import { XMLParser } from 'fast-xml-parser'
-import { toStringArray } from './src/utils/xmlHelper'
-import { ICrew } from '@Types/Vehicle'
+import { ISpeedLimit, ICamo } from '@Types/Modules'
+import { toStringArray, toNumber, toNumberArray } from '@Utils/xmlHelper'
+
+import { ReturnHull } from '@VehicleParts/Hull/HullArmor'
 
 const parser = new XMLParser({
    ignoreAttributes: false,
@@ -13,33 +15,48 @@ const parser = new XMLParser({
 })
 
 const filePath = path.join('./XML/A182_T803.xml')
+// const manticoreFilePath = path.join('./XML/GB100_Manticore.xml')
 const xmlString = fs.readFileSync(filePath, 'utf-8')
 const convertedRawJSON = parser.parse(xmlString)
 
 const fileName = path.basename(filePath)
 
-// fs.writeFileSync(path.join(`vehicles.json`), JSON.stringify(convertedJSON, null, 2), 'utf8')
-
 function GetCrewData(rawJSON: any) {
    const crewData = []
-
    if (rawJSON?.crew) {
-      // console.log('RAW: ', rawJSON?.crew)
       for (const [key, value] of Object.entries(rawJSON.crew)) {
-         console.log(key, value)
          crewData.push({
             primary: key,
             secondary: toStringArray(value),
          })
       }
    }
-
-   console.log(crewData)
+   return crewData
 }
 
-// console.log(convertedRawJSON[fileName])
-// console.log(fileName)
+function ReturnSpeedLimits(rawJSON: any) {
+   const speedLimits: ISpeedLimit = { backward: 0, forward: 0 }
+   if (rawJSON?.speedLimits) {
+      speedLimits.forward = toNumber(rawJSON.speedLimits?.forward) || 0
+      speedLimits.backward = toNumber(rawJSON.speedLimits?.backward) || 0
+   }
+   return speedLimits
+}
+
+function ReturnCamoValues(rawJSON: any) {
+   const camoValues: ICamo = { moving: 0, stationary: 0, camoBonus: 0, firePenalty: 0 }
+   if (rawJSON?.invisibility) {
+      camoValues.moving = toNumber(rawJSON.invisibility?.moving) || 0
+      camoValues.stationary = toNumber(rawJSON.invisibility?.still) || 0
+      camoValues.camoBonus = toNumber(rawJSON.invisibility?.camouflageBonus) || 0
+      camoValues.firePenalty = toNumber(rawJSON.invisibility?.firePenalty) || 0
+   }
+   return camoValues
+}
 
 GetCrewData(convertedRawJSON[fileName])
+ReturnSpeedLimits(convertedRawJSON[fileName])
+ReturnCamoValues(convertedRawJSON[fileName])
+console.log(ReturnHull(convertedRawJSON[fileName]))
 
 console.log('RAN')
