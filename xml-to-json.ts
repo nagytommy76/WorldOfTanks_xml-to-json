cfdc93af494981f6c9e2c1fecd923e3aafdf64fd
nagytommy76 <nagytommy76@gmail.com>
2025-11-10 20:03:11 +0100

    Use the return functions

--- xml-to-json.ts
+++ xml-to-json.ts
@@ -1,12 +1,16 @@
 import fs from 'fs'
 import path from 'path'
-import { ISpeedLimit, ICamo } from '@Types/Modules'
-import { toStringArray, toNumber, toNumberArray } from '@Utils/xmlHelper'
 import xmlParser from '@Utils/xmlParser'
 
 import { ReturnHull } from '@VehicleParts/Hull/HullArmor'
 import { ReturnChassis } from '@VehicleParts/Chassis/ReturnChassis'
 import ReturnTurrets from '@VehicleParts/Turrets/Turrets'
+import ReturnSpeedLimits from '@VehicleParts/Other/SpeedLimits'
+import ReturnCamoValues from '@VehicleParts/Other/Camo'
+import GetCrewData from '@VehicleParts/Other/Crew'
+import ReturnEngines from '@VehicleParts/Other/Engines'
+import ReturnFuelTanks from '@VehicleParts/Other/FuelTank'
+import ReturnRadios from '@VehicleParts/Other/Radio'
 
 // const filePath = path.join('./XML/A182_T803.xml')
 const filePath = path.join('./XML/R19_IS-3.xml')
@@ -16,44 +20,15 @@ const convertedRawJSON = xmlParser.parse(xmlString)
 
 const fileName = path.basename(filePath)
 
-function GetCrewData(rawJSON: any) {
-   const crewData = []
-   if (rawJSON?.crew) {
-      for (const [key, value] of Object.entries(rawJSON.crew)) {
-         crewData.push({
-            primary: key,
-            secondary: toStringArray(value),
-         })
-      }
-   }
-   return crewData
-}
-
-function ReturnSpeedLimits(rawJSON: any) {
-   const speedLimits: ISpeedLimit = { backward: 0, forward: 0 }
-   if (rawJSON?.speedLimits) {
-      speedLimits.forward = toNumber(rawJSON.speedLimits?.forward) || 0
-      speedLimits.backward = toNumber(rawJSON.speedLimits?.backward) || 0
-   }
-   return speedLimits
-}
-
-function ReturnCamoValues(rawJSON: any) {
-   const camoValues: ICamo = { moving: 0, stationary: 0, camoBonus: 0, firePenalty: 0 }
-   if (rawJSON?.invisibility) {
-      camoValues.moving = toNumber(rawJSON.invisibility?.moving) || 0
-      camoValues.stationary = toNumber(rawJSON.invisibility?.still) || 0
-      camoValues.camoBonus = toNumber(rawJSON.invisibility?.camouflageBonus) || 0
-      camoValues.firePenalty = toNumber(rawJSON.invisibility?.firePenalty) || 0
-   }
-   return camoValues
-}
-
-GetCrewData(convertedRawJSON[fileName])
-ReturnSpeedLimits(convertedRawJSON[fileName])
-ReturnCamoValues(convertedRawJSON[fileName])
 ReturnHull(convertedRawJSON[fileName])
 ReturnChassis(convertedRawJSON[fileName])
 ReturnTurrets(convertedRawJSON[fileName])
+
+GetCrewData(convertedRawJSON[fileName])
+ReturnCamoValues(convertedRawJSON[fileName])
+ReturnSpeedLimits(convertedRawJSON[fileName])
+ReturnEngines(convertedRawJSON[fileName])
+ReturnFuelTanks(convertedRawJSON[fileName])
+ReturnRadios(convertedRawJSON[fileName])
 
 console.log('RAN')
