45c30f1c861d162145ca4ada032528e6a5114ebe
nagytommy76 <nagytommy76@gmail.com>
2025-11-11 20:34:06 +0100

    Separate ReturnSingleVehicle function

---
+++ src/ReturnSingleVehicle.ts
@@ -0,0 +1,52 @@
+import type { ITankData } from '@Types/Modules'
+
+import { ReturnHull } from '@VehicleParts/Hull/HullArmor'
+import { ReturnChassis } from '@VehicleParts/Chassis/ReturnChassis'
+import ReturnTurrets from '@VehicleParts/Turrets/Turrets'
+import ReturnSpeedLimits from '@VehicleParts/Other/SpeedLimits'
+import ReturnCamoValues from '@VehicleParts/Other/Camo'
+import GetCrewData from '@VehicleParts/Other/Crew'
+import ReturnEngines from '@VehicleParts/Other/Engines'
+import ReturnFuelTanks from '@VehicleParts/Other/FuelTank'
+import ReturnRadios from '@VehicleParts/Other/Radio'
+import ReturnOtherData from '@VehicleParts/Other/OtherData'
+
+export default function ReturnSingleVehicle(
+   convertedRawJSON: any,
+   fileName: string,
+   shortName: string,
+   nation: string = 'ussr',
+   nationDir: string = 'ussr'
+): ITankData {
+   const hullData = ReturnHull(convertedRawJSON[fileName])
+   const chassisData = ReturnChassis(convertedRawJSON[fileName])
+   const turretsData = ReturnTurrets(convertedRawJSON[fileName], nationDir)
+
+   const crew = GetCrewData(convertedRawJSON[fileName])
+   const camo = ReturnCamoValues(convertedRawJSON[fileName])
+   const speedLimits = ReturnSpeedLimits(convertedRawJSON[fileName])
+   const engines = ReturnEngines(convertedRawJSON[fileName], nationDir)
+   const fuelTank = ReturnFuelTanks(convertedRawJSON[fileName], nationDir)
+   const radios = ReturnRadios(convertedRawJSON[fileName], nationDir)
+
+   const otherData = ReturnOtherData(convertedRawJSON[fileName])
+   const Vehicle: ITankData = {
+      ...otherData,
+   } as ITankData
+
+   Vehicle.stats = {
+      camo: camo,
+      chassis: chassisData,
+      engines: engines,
+      fuelTank: fuelTank,
+      hull: hullData,
+      radios: radios,
+      speedLimit: speedLimits,
+      turrets: turretsData,
+   }
+   Vehicle.name = shortName
+   Vehicle.nation = nation
+   Vehicle.crew = crew
+
+   return Vehicle
+}
