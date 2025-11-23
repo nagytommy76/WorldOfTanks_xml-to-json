8e0eeb8e0faf2ab0a3afca8b8199ce0a3e7e8bb1
nagytommy76 <nagytommy76@gmail.com>
2025-11-23 13:23:33 +0100

    separate 2 constants
    fileNameStartsWithByNations & notToIncludeFileNames

---
+++ src/utils/consts.ts
@@ -0,0 +1,42 @@
+/**
+ * @description List of file names to exclude
+ */
+export const notToIncludeFileNames = [
+   'bob',
+   'Dummy',
+   'MapsTraining',
+   '7x7',
+   'IGR',
+   'FL',
+   'CN',
+   'NewOnBoarding',
+   'StoryModeStealth',
+   'StoryMode',
+   'fallout',
+   'GrandFinal',
+   'SH',
+   'LE',
+   'CO',
+   'CFE',
+   'Pillbox',
+   'Bomber',
+]
+
+/**
+ * @description Key value pair of file name starts with and nation
+ * @param key nation
+ * @param value file name starts with the letter in XML folder
+ */
+export const fileNameStartsWithByNations = {
+   // germany: 'G',
+   // ussr: 'R',
+   // usa: 'A',
+   // france: 'F',
+   // uk: 'GB',
+   // china: 'Ch',
+   // japan: 'J',
+   // czech: 'Cz',
+   // poland: 'Pl',
+   sweden: 'S',
+   // italy: 'It',
+}
