9dfa93ff14949c4f84cc2c9cb33e9f3bd50e0a0b
nagytommy76 <nagytommy76@gmail.com>
2025-12-26 19:51:36 +0100

    Separate hydropneumatic & siegemode

--- src/VehicleParts/Other/SiegeMode.ts
+++ src/VehicleParts/Other/SiegeMode.ts
@@ -2,19 +2,26 @@ import { toNumber } from '@/src/utils/xmlHelper'
 import { ISiegeMode, IHydropneumatic } from '@Types/Modules'
 
 export default function ReturnSiegeMode(rawJSON: any) {
-   if (rawJSON?.siegeMode && rawJSON.hull_aiming?.pitch) {
-      const siegeMode: ISiegeMode | null = { switchOffTime: 0, switchOnTime: 0 }
-      const hydropneumatic: IHydropneumatic | null = { depression: 0, elevation: 0 }
-
+   let siegeMode: ISiegeMode | null = { switchOffTime: 0, switchOnTime: 0 }
+   if (rawJSON?.siegeMode) {
       siegeMode.switchOffTime = toNumber(rawJSON.siege_mode?.switchOffTime) || 0
       siegeMode.switchOnTime = toNumber(rawJSON.siege_mode?.switchOnTime) || 0
 
+      return siegeMode
+   } else {
+      siegeMode = null
+
+      return siegeMode
+   }
+}
+
+export function ReturnHydropneumatic(rawJSON: any) {
+   let hydropneumatic: IHydropneumatic | null = { depression: 0, elevation: 0 }
+   if (rawJSON.hull_aiming?.pitch) {
       hydropneumatic.depression = toNumber(rawJSON.hull_aiming.pitch.wheelsCorrectionAngles.pitchMin) || 0
       hydropneumatic.elevation = toNumber(rawJSON.hull_aiming.pitch.wheelsCorrectionAngles.pitchMax) || 0
-      return { siegeMode, hydropneumatic }
    } else {
-      const siegeMode: ISiegeMode | null = null
-      const hydropneumatic: IHydropneumatic | null = null
-      return { siegeMode, hydropneumatic }
+      hydropneumatic = null
    }
+   return hydropneumatic
 }
