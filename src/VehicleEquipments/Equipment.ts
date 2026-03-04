5d0a9d4a06e74cf213d19cac561c0fd97205a5ad
nagytommy76 <nagytommy76@gmail.com>
2026-03-04 19:57:09 +0100

    Add interfaces
    IModifier, IVehicleNationFilter, XMLModifiers

--- src/VehicleEquipments/Equipment.ts
+++ src/VehicleEquipments/Equipment.ts
@@ -1,4 +1,25 @@
 /**
+ * @description Modifier values from XML -> kpi -> mul -> name/value
+ */
+interface IModifier {
+   name: string
+   value: number
+}
+
+/**
+ * @description Nation filter for extra rations ( cocacola, chocolate, etc.)
+ */
+interface IVehicleNationFilter {
+   nation: string
+}
+
+type XMLModifiers = {
+   kpi: {
+      mul: IModifier[]
+   }
+}
+
+/**
  * @param {number} id provision_id from WG API
  * @param {string} icon icon name (use for find icons in public/icons folder (largeMedkit.jpg))
  * @param {number} price credit price
@@ -13,7 +34,9 @@ export default class Equipment {
    name: string
    tag: string
    description: string
-   declare test: number
+
+   private modifiers?: IModifier[]
+   private nationFilters?: IVehicleNationFilter[]
 
    constructor(id: number, icon: string, price: number, name: string, description: string, tag: string) {
       this.id = id
@@ -24,7 +47,34 @@ export default class Equipment {
       this.tag = tag
    }
 
-   setTest() {
-      this.test = 1
+   private setModifiers(modifiers: IModifier[]) {
+      this.modifiers = modifiers
    }
+
+   private setNationFilters(nationFilters: IVehicleNationFilter[]) {
+      this.nationFilters = nationFilters
+   }
+
+   getModifiers() {
+      return this.modifiers
+   }
+
+   getNationFilters() {
+      return this.nationFilters
+   }
+
+   setModifiersFromXML(xmlModifiers: XMLModifiers) {}
 }
+
+/**
+ *   <kpi>
+      <mul>
+        <name>	crewHitChance	</name>
+        <value>	1.15	</value>
+      </mul>
+      <mul>
+        <name>	crewStunDuration	</name>
+        <value>	0.95	</value>
+      </mul>
+    </kpi>
+ */
