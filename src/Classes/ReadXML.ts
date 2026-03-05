7333e8a2897206fe72e93839dfc0a123c7254976
nagytommy76 <nagytommy76@gmail.com>
2026-03-05 20:01:05 +0100

    add returnNationXMLFiles function to ReadXML class

--- src/Classes/ReadXML.ts
+++ src/Classes/ReadXML.ts
@@ -1,15 +1,26 @@
 import fs from 'fs'
 import path from 'path'
+import type { NationType } from '@Types/Vehicle'
+import { fileNameStartsWithByNations, notToIncludeFileNames } from '@Utils/consts'
 
 export default class ReadXML {
-   xmlSourceDirectory: string = path.resolve('./XML')
-   jsonOutputDirectory: string = path.resolve('./JSON')
+   nation: NationType
+   xmlSourceDirectory: string
+   jsonOutputDirectory: string
 
    nationSourceXMLDirectory: string
    nationOutputJSONDirectory: string
 
-   constructor(currentNation: string) {
+   constructor(
+      currentNation: NationType,
+      jsonOutputDirectory: string = './JSON',
+      xmlSourceDirectory: string = './XML',
+   ) {
+      this.nation = currentNation
+      this.xmlSourceDirectory = xmlSourceDirectory
+      this.jsonOutputDirectory = jsonOutputDirectory
       this.CheckOutputJSONDirectory()
+
       this.nationSourceXMLDirectory = path.join(this.xmlSourceDirectory, currentNation)
       this.nationOutputJSONDirectory = path.join(this.jsonOutputDirectory, currentNation)
       this.CheckOutputNationJSONDirectory()
@@ -31,5 +42,30 @@ export default class ReadXML {
       if (!fs.existsSync(this.jsonOutputDirectory)) {
          fs.mkdirSync(this.jsonOutputDirectory, { recursive: true })
       }
+   }
+
+   /**
+    * @returns {string[]} Returns an array of all the nation's XML files
+    */
+   returnNationXMLFiles() {
+      const nationXMLFiles = fs
+         .readdirSync(this.nationSourceXMLDirectory)
+         .filter((file) => file.toLowerCase().endsWith('.xml'))
+         //   Include all of the nation's vehicles / not list.xml for example
+         .filter((file) => {
+            return file.startsWith(fileNameStartsWithByNations[this.nation])
+         })
+         // exclude training / special mode vehicles
+         .filter((file) => {
+            const base = path.basename(file, '.xml')
+            return !notToIncludeFileNames.some((bad) => base.includes(bad))
+         })
+
+      if (nationXMLFiles.length === 0) {
+         console.log('No XML files found in directory: ', this.nationSourceXMLDirectory)
+         process.exit(0)
+      }
+
+      return nationXMLFiles
    }
 }
