fa236417cb7202dd2d4acc4ec553901d4d14eff6
nagytommy76 <nagytommy76@gmail.com>
2026-03-06 19:57:07 +0100

    Remove tag (same as icon), add setModifiers function

--- src/VehicleEquipments/Equipment.ts
+++ src/VehicleEquipments/Equipment.ts
@@ -3,20 +3,15 @@
  */
 interface IModifier {
    name: string
-   value: number
+   value: number | string
 }
 
-/**
- * @description Nation filter for extra rations ( cocacola, chocolate, etc.)
- */
-interface IVehicleNationFilter {
-   nation: string
-}
-
-type XMLModifiers = {
-   kpi: {
-      mul: IModifier[]
-   }
+interface EquipmentOptions {
+   id: number
+   icon: string
+   price: number
+   name: string
+   description: string
 }
 
 /**
@@ -25,56 +20,32 @@ type XMLModifiers = {
  * @param {number} price credit price
  * @param {string} name full name of the equipment (Manual Fire Extinguisher from WG API)
  * @param {string} description description of the equipment from WG API
- * @param {string} tag XML name of the equipment (largeMedkit, handExtinguishers ration_poland)
  */
 export default class Equipment {
    id: number
    icon: string
    price: number
    name: string
-   tag: string
    description: string
 
-   private modifiers?: IModifier[]
-   private nationFilters?: IVehicleNationFilter[]
+   modifiers: IModifier[] = []
+   nationFilter?: string
 
-   constructor(id: number, icon: string, price: number, name: string, description: string, tag: string) {
+   constructor({ description, icon, id, name, price }: EquipmentOptions) {
       this.id = id
       this.icon = icon
       this.price = price
       this.name = name
       this.description = description
-      this.tag = tag
    }
 
-   private setModifiers(modifiers: IModifier[]) {
-      this.modifiers = modifiers
+   setModifiers(modifier: IModifier[] | IModifier) {
+      const normalizedModifier = Array.isArray(modifier) ? modifier : [modifier]
+      this.modifiers.push(
+         ...normalizedModifier.map((m) => ({
+            name: m.name,
+            value: Number(m.value),
+         })),
+      )
    }
-
-   private setNationFilters(nationFilters: IVehicleNationFilter[]) {
-      this.nationFilters = nationFilters
-   }
-
-   getModifiers() {
-      return this.modifiers
-   }
-
-   getNationFilters() {
-      return this.nationFilters
-   }
-
-   setModifiersFromXML(xmlModifiers: XMLModifiers) {}
 }
-
-/**
- *   <kpi>
-      <mul>
-        <name>	crewHitChance	</name>
-        <value>	1.15	</value>
-      </mul>
-      <mul>
-        <name>	crewStunDuration	</name>
-        <value>	0.95	</value>
-      </mul>
-    </kpi>
- */
