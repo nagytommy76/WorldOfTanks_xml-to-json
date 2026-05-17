bdd29540c77b6dfed0cbb5d7e4176cce23a8ef2a
nagytommy76 <nagytommy76@gmail.com>
2026-05-17 12:11:04 +0200

    Upload crew skills to database

--- CrewSkills/UploadCrewSkills.ts
+++ CrewSkills/UploadCrewSkills.ts
@@ -1,51 +1,53 @@
 import type { XMLCrewSKills } from '@Types/CrewSkills'
 
 import CrewSkills, { type CrewSkillsType, type CrewSkillModifier } from '@/Classes/CrewSkills'
+import fetchCrewSkills from '@Utils/fetchCrewSkills'
+import { CrewSkillModel } from '@Models/CrewSkillModel'
+import connectDB from '@/Config/connectDB'
 
 import parseXMLFile from '@/src/VehicleEquipments/ParseXMLFile'
+import skillDescriptions from './SkillDescriptions'
+import AddModifiersArray from './AddModifierArray'
 
 export default async function UploadCrewSkills() {
-   const crewSkillsXML = await parseXMLFile<{ skills: XMLCrewSKills }>('./XML/common/tankmen.xml')
+   await connectDB()
+   try {
+      const crewSkillsXML = await parseXMLFile<{ skills: XMLCrewSKills }>('./XML/common/tankmen.xml')
+      const fetchedCrewSkills = await fetchCrewSkills()
+      for (let [skillName, skill] of Object.entries(crewSkillsXML.skills)) {
+         if (skillName === 'commander_sixthSense' || skillName === 'armorPatching') continue
+         if (skillName === 'fireFighting') skillName = 'radioman_fireFighting'
+         const role =
+            skill.UISettings.typeName === 'common'
+               ? 'common'
+               : (skillName.split('_')[0] as CrewSkills['role'])
 
-   // console.log(crewSkillsXML.skills.gunner_rancorous.UISettings)
-   // console.log(crewSkillsXML.skills.gunner_focus.UISettings.descr.arg)
-
-   for (const [skillName, skill] of Object.entries(crewSkillsXML.skills)) {
-      let modifiers: CrewSkillModifier[] | undefined = undefined
-      modifiers = []
-      if (Array.isArray(skill.UISettings.descr.arg)) {
-         skill.UISettings.descr.arg.forEach((argument) => {
-            modifiers?.push({
-               measureType: argument.measureType as 'percent' | 'seconds',
-               paramName: argument.paramName,
-               value:
-                  Number(argument.value) >= 0
-                     ? Number(argument.value) * 100 + 1
-                     : Number(argument.value) * 100,
-               situationalParam: Boolean(argument.situationalParam),
+         let modifiers: CrewSkillModifier[] | undefined = undefined
+         modifiers = []
+         if (Array.isArray(skill.UISettings.descr.arg)) {
+            skill.UISettings.descr.arg.forEach((argument) => {
+               AddModifiersArray(modifiers, argument)
             })
+         } else {
+            AddModifiersArray(modifiers, skill.UISettings.descr.arg)
+         }
+
+         const skillInstance = new CrewSkills({
+            description: skillDescriptions[skillName],
+            name: fetchedCrewSkills[skillName]?.name || null,
+            role,
+            typeName: skill.UISettings.typeName as CrewSkillsType,
+            xmlName: skillName,
+            modifiers: modifiers,
          })
-      } else {
-         modifiers.push({
-            measureType: skill.UISettings.descr.arg.measureType as 'percent' | 'seconds',
-            paramName: skill.UISettings.descr.arg.paramName,
-            value:
-               Number(skill.UISettings.descr.arg.value) >= 0
-                  ? Number(skill.UISettings.descr.arg.value) * 100 + 1
-                  : Number(skill.UISettings.descr.arg.value) * 100,
-            situationalParam: Boolean(skill.UISettings.descr.arg.situationalParam),
-         })
+
+         const crewModel = new CrewSkillModel(skillInstance)
+
+         await crewModel.save()
+
+         console.log(`${crewModel.xmlName} has been succesfully uploaded to DB`)
       }
-
-      const skillInstance = new CrewSkills({
-         description: 'desc FROM WG API',
-         name: 'name FROM WG API',
-         role: skillName.split('_')[0] as CrewSkills['role'],
-         typeName: skill.UISettings.typeName as CrewSkillsType,
-         xmlName: skillName,
-         modifiers: modifiers,
-      })
-
-      console.log(skillName, skillInstance)
+   } catch (error) {
+      console.error(error)
    }
 }
